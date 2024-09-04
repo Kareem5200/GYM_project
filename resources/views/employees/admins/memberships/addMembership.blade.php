ed73ee09c864e68520133091bf0e1b1a9ec5b372
Kareem5200 <karim8862@gmail.com>
2024-09-04 16:21:31 +0300

    Kareem commit

---
+++ resources/views/employees/admins/memberships/addMembership.blade.php
@@ -0,0 +1,17 @@
+@extends('employees.layouts.navbar')
+
+@section('title')
+<title> Add Membership</title>
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('js')
+
+@endsection
---
+++ resources/views/employees/admins/memberships/memberships.blade.php
@@ -0,0 +1,17 @@
+@extends('employees.layouts.navbar')
+
+@section('title')
+<title>Memberships</title>
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('js')
+
+@endsection
